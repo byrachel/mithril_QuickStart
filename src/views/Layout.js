cfa62c729640a5c1be681ecf1185a14825b4798a
Rachel Nething <byrachel@gmail.com>
2020-03-23 09:26:32 +0100

    event clic added

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,12 +1,18 @@
-var m = require("mithril")
+var m = require("mithril");
+
+var count = 0;
 
 module.exports = {
+    
     view: function(vnode) {
         return m("main.layout", [
+            m("h1", { class: "title"}, "Mithril JS"),
+            m("h2", { class: "subtitle"}, "demo"),
+            m("button", {onclick: function() {count++}}, count + " clic"),
             m("nav.menu", [
                 m(m.route.Link, {href: "/list"}, "Users")
             ]),
-            m("section", vnode.children)
+            m("section", vnode.children),
         ])
     }
 }
